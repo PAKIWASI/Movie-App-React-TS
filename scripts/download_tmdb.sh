367dbf10f70dbe58c5ca4a52ba6da5b442ad8bf3
PAKIWASI <ullahwasi26@gmail.com>
2026-03-18 13:16:59 +0500

    getting data

--- scripts/download_tmdb.sh
+++ scripts/download_tmdb.sh
@@ -3,14 +3,19 @@
 
 source ../.env
 
-
 API_KEY=$VITE_TMDB_API_KEY;
 BASE_URL="https://api.themoviedb.org/3"
 
-# popular movies
-PAGE=1
-curl "$BASE_URL/movie/popular?api_key=$API_KEY&$PAGE=1" | jq
 
-# movie details
-ID=1265609
-curl "$BASE_URL/movie/$ID?api_key=$API_KEY&language=en-US" | jq
+for PAGE in {1..10} do
+    # popular movies pages
+    RESPONSE=$(curl -s "$BASE_URL/movie/popular?api_key=$API_KEY&page=$PAGE")
+    echo $RESPONSE | jq
+
+
+    # Get specific id for each movie
+    ID=$(echo $RESPONSE | jq '.results[1].id')
+    # movie details
+    DETAIL=$(curl -s "$BASE_URL/movie/$ID?api_key=$API_KEY&language=en-US")
+    echo $DETAIL | jq
+done 

6011824b4c398ec944ced15c6068eb28c3a6f49a
PAKIWASI <ullahwasi26@gmail.com>
2026-03-18 12:59:49 +0500

    get popular movie and movie details

--- scripts/download_tmdb.sh
+++ scripts/download_tmdb.sh
@@ -7,5 +7,10 @@ source ../.env
 API_KEY=$VITE_TMDB_API_KEY;
 BASE_URL="https://api.themoviedb.org/3"
 
+# popular movies
+PAGE=1
+curl "$BASE_URL/movie/popular?api_key=$API_KEY&$PAGE=1" | jq
 
-curl "$BASE_URL/movie/popular?api_key=$API_KEY&page=1" | jq
+# movie details
+ID=1265609
+curl "$BASE_URL/movie/$ID?api_key=$API_KEY&language=en-US" | jq

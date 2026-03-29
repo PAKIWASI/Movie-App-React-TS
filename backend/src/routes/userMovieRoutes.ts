171eea86be6e9df479a20e7ba254f097c2bf01e5
PAKIWASI <ullahwasi26@gmail.com>
2026-03-30 03:10:44 +0500

    adding fav/watchlist

--- backend/src/routes/userMovieRoutes.ts
+++ backend/src/routes/userMovieRoutes.ts
@@ -9,14 +9,17 @@ const router = Router();
 // GET /api/user/:id/movie
 // get all movies associated with user
 // GET /api/user/:userId/movie?inFavs=true&inWatchlist=true&watched=false&page=1&limit=10&name=inception
-router.get("/",                      getUserMovies);        // GET  /api/user/:id/movie
-router.post("/",                     postUserMovie);        // POST /api/user/:id/movie
+router.get("/",                     getUserMovies);    // GET  /api/user/:id/movie
+router.post("/",                    postUserMovie);    // POST /api/user/:id/movie
 
-router.put("/:tmdbId",               updateUserMovie);      // PUT  /api/user/:id/movie/:tmdbId
-router.delete("/:tmdbId",            deleteUserMovie);      // DELETE
+router.put("/:tmdbId",              updateUserMovie);  // PUT  /api/user/:id/movie/:tmdbId
+router.delete("/:tmdbId",           deleteUserMovie);  // DELETE
 
-router.patch("/:tmdbId/watchlist",   toggleWatchlist);      // PATCH /api/user/:id/movie/:tmdbId/watchlist
-router.patch("/:tmdbId/favorites",   toggleFavorites);      // PATCH
+router.patch("/:tmdbId/watchlist",  toggleWatchlist);  // PATCH /api/user/:id/movie/:tmdbId/watchlist
+router.patch("/:tmdbId/favorites",  toggleFavorites);  // PATCH
+router.patch("/:tmdbId/watched",    toggleWatched);    // PATCH
+router.patch("/:tmdbId/rating",     setRating);        // PATCH
+router.patch("/:tmdbId/review",     setReview);        // PATCH
 // using patch to toggle watchlist, favlist (put implies whole object changed)
 
 export default router;
--- backend/src/types/user_movie.type.ts
+++ backend/src/types/user_movie.type.ts
@@ -4,11 +4,11 @@ import z from "zod"
 
 
 export const UserMovieSchema = z.object({
-    userId: z.string().refine(val => mongoose.Types.ObjectId.isValid(val), "Invalid ObjectId"),
-    tmdbId:         z.number(),     // ref to movie
-    inFavs:         z.boolean().default(false),
-    inWatchlist:    z.boolean().default(false),
-    watched:        z.boolean().default(false),
+    userId:      z.string().refine(val => mongoose.Types.ObjectId.isValid(val), "Invalid ObjectId"),
+    tmdbId:      z.number(),     // ref to movie
+    inFavs:      z.boolean().default(false),
+    inWatchlist: z.boolean().default(false),
+    watched:     z.boolean().default(false),
     userRating:  z.number().min(0).max(10).default(0),
     userReview:  z.string().default(""),
 });
